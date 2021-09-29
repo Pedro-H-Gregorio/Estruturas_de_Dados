fd6bd706113c7f25c1f2060fba299d06b2631893
Pedro Henrique Gregório <pedro.gregorio@academico.ifpb.edu.br>
2021-09-29 20:11:23 -0300

    Salando questões respondidas com sucesso

--- Uri_Estrutura_de_dados/Pilhas/1068_uri.js
+++ Uri_Estrutura_de_dados/Pilhas/1068_uri.js
@@ -1,68 +1,53 @@
 class Pilha {
 
-    constructor() {
-      this.lista = new Array()
-    }
-  
-    push(e) {
-      return this.lista[this.lista.length]=e
-    }
-  
-    pop() {
-      let valor = this.isEmpty()? null : this.lista[this.lista.length - 1]
-      this.lista.splice(this.lista.length - 1,1)
-      return valor
-    }
-  
-    size() {
-      return this.lista.length;
-    }
-  
-    isEmpty() {
-      return this.lista.length == 0 ? true : false ;
-    }
-  
-    top() {
-      return this.isEmpty() ? null : this.lista[this.lista.length - 1]
-    }
+  constructor() {
+    this.lista = new Array()
   }
+
+  push(e) {
+    return this.lista[this.lista.length]=e
+  }
+
+  pop() {
+    let valor = this.isEmpty()? null : this.lista[this.lista.length - 1]
+    this.lista.splice(this.lista.length - 1,1)
+    return valor
+  }
+
+  size() {
+    return this.lista.length;
+  }
+
+  isEmpty() {
+    return this.lista.length == 0 ? true : false ;
+  }
+
+  top() {
+    return this.isEmpty() ? null : this.lista[this.lista.length - 1]
+  }
+}
   
 var input = require('fs').readFileSync('stdin', 'utf8').split("\n");
-function procurar (array, n) {
-    let cont;
-    for (let i in array){
-        let j = parseInt(i)
-       switch( array.substr(j,1)){
-           case n : {
-               cont = j
-               array - n
-           }
-           break
-       }
-    }
-    return cont
-}
 
 for (let expressao of input){
-    let exp = expressao.trim()
-    let pilha = new Pilha()
-    let valores = "";
-    console.log("pedro" - "p")
-    for (let caracter of exp){
-        if ((caracter == '(') || caracter == ')'){
-            valores += caracter
-        }
-    }
-    if((valores.substring(0,1)==")") || (valores.substr(valores.length-1,1)=="(")){console.log("incorrect")}
-    else
-    {
-        for(let aspas of valores){
-            pilha.push(aspas)
-            if((pilha.top()=="(") && (procurar(valores,")")!="nao achado")){
-                let palavra = pilha.pop() + valores.slice(procurar(valores,")"),procurar(valores,")")+1)
-            }if(palavra == '()'){
+  let exp = expressao.trim()
+  let pilha = new Pilha()
+  let aspas = ""
 
-            }
-        }
+  for (let caracter of exp){
+    aspas += caracter == "(" || caracter == ")"? caracter : ""
+    let value = false
+    if ((caracter == '(')){
+        pilha.push(caracter)
     }
+    else if((caracter == ")") && (pilha.top() == '(') && (pilha.isEmpty()!= true)){
+      pilha.pop()
+      value = true
+    } else if (caracter == ")" && value == false){
+      pilha.push(caracter)
+    }
+  }
+  if(aspas.length > 0){
+  console.log((pilha.isEmpty())? "correct": "incorrect")
+  }
 }
--- Uri_Estrutura_de_dados/Pilhas/1069_uri.js
+++ Uri_Estrutura_de_dados/Pilhas/1069_uri.js
@@ -1,29 +1,46 @@
 class Pilha {
 
-    constructor() {
-      this.lista = new Array()
-    }
-  
-    push(e) {
-      return this.lista[this.lista.length]=e
-    }
-  
-    pop() {
-      let valor = this.isEmpty()? null : this.lista[this.lista.length - 1]
-      this.lista.splice(this.lista.length - 1,1)
-      return valor
-    }
-  
-    size() {
-      return this.lista.length;
-    }
-  
-    isEmpty() {
-      return this.lista.length == 0 ? true : false ;
-    }
-  
-    top() {
-      return this.isEmpty() ? null : this.lista[this.lista.length - 1]
+  constructor() {
+    this.lista = new Array()
+  }
+
+  push(e) {
+    return this.lista[this.lista.length]=e
+  }
+
+  pop() {
+    let valor = this.isEmpty()? null : this.lista[this.lista.length - 1]
+    this.lista.splice(this.lista.length - 1,1)
+    return valor
+  }
+
+  size() {
+    return this.lista.length;
+  }
+
+  isEmpty() {
+    return this.lista.length == 0 ? true : false ;
+  }
+
+  top() {
+    return this.isEmpty() ? null : this.lista[this.lista.length - 1]
+  }
+}
+var input = require('fs').readFileSync('stdin', 'utf8').split("\n");
+let cont = parseInt(input.shift())
+
+for (let i = 0; i < cont ; i++){
+  let pilha = new Pilha()
+  let valores = input.shift().trim()
+  let diamantes = 0
+
+  for (let j of valores){
+    if (j == "<"){
+      pilha.push(j)
+    }else if ((j == ">") && (pilha.isEmpty() != true)) {
+      pilha.pop()
+      diamantes++
     }
   }
-  
+  console.log(diamantes)
+}
