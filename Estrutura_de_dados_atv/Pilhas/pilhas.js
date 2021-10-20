ac3f8370d4949ece9c8424718bf0b2d479a6b6b5
Pedro Henrique Gregório <pedro.gregorio@academico.ifpb.edu.br>
2021-10-20 10:51:45 -0300

    Ajeitando

--- Estrutura_de_dados_atv/Pilhas/pilhas.js
+++ Estrutura_de_dados_atv/Pilhas/pilhas.js
@@ -1,19 +1,18 @@
 // Documentação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
 
 class Pilha {
-
   constructor() {
-    this.lista = new Array()
+    this.lista = new Array();
   }
 
   push(e) {
-    return this.lista[this.lista.length]=e
+    return (this.lista[this.lista.length] = e);
   }
 
   pop() {
-    let value = this.isEmpty()? null : this.lista[this.lista.length - 1]
-    this.lista.splice(this.lista.length - 1,1)
-    return value
+    let value = this.isEmpty() ? null : this.lista[this.lista.length - 1];
+    this.lista.splice(this.lista.length - 1, 1);
+    return value;
   }
 
   size() {
@@ -21,11 +20,11 @@ class Pilha {
   }
 
   isEmpty() {
-    return this.lista.length == 0 ? true : false ;
+    return this.lista.length == 0 ? true : false;
   }
 
   top() {
-    return this.isEmpty() ? null : this.lista[this.lista.length - 1]
+    return this.isEmpty() ? null : this.lista[this.lista.length - 1];
   }
 }
 
