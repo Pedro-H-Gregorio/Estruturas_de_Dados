01d0745aa04bc61a8f3568440032be0ce27a1a47
Pedro Henrique Gregório <pedro.gregorio@academico.ifpb.edu.br>
2021-10-26 02:10:35 -0300

    Alterando para ficar apenas o metodo

--- Estrutura_de_dados_atv/Fila/filas.js
+++ Estrutura_de_dados_atv/Fila/filas.js
@@ -36,20 +36,3 @@ class Fila {
 }
 
 module.exports = Fila;
-
-let fila = new Fila();
-console.log(fila.isEmpty());
-fila.enqueue(1);
-fila.enqueue(2);
-fila.enqueue(3);
-fila.enqueue(4);
-console.log(fila.front());
-console.log(fila.lista);
-fila.dequeue();
-console.log(fila.lista);
-console.log(fila.front());
-fila.dequeue();
-fila.dequeue();
-console.log(fila.lista);
-console.log(fila.front());
-console.log(fila.isEmpty());
